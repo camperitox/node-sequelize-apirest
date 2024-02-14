b95027171833a2fe99bc951e8230349f611f5bd6
camperitox <ml17017@ues.edu.sv>
2024-02-14 15:19:05 -0600

    Sequelize Schemas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import app from './app.js';
+import { sequelize } from './database/database.js';
+import './models/Project.js';
+import './models/Task.js';
 
-app.listen(3000)
-console.log('Server on port 3000')
+async function main() {
+    try {
+        await sequelize.sync({force:true})// esto crea las tablas si no existen aun 
+        app.listen(3000)
+        console.log('Server on port 3000')
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+main();
---
+++ src/models/Project.js
@@ -0,0 +1,38 @@
+import {DataTypes} from 'sequelize';
+import { sequelize } from '../database/database.js';
+import { Task } from './Task.js';
+
+// Aqui definimos comO queremos que se llame la tabla y los campos que debe tener esa tabla
+// Basicamente este es el esquema de la tabla
+export const Project = sequelize.define('project', {
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true, // definimos que es una llave primaria
+        autoIncrement: true
+    },
+    name:{
+        type: DataTypes.STRING
+    },
+    priority:{
+        type: DataTypes.INTEGER
+    },
+    description:{
+        type: DataTypes.STRING
+    },
+})
+
+
+// Creamos las relaciones entre las tablas
+
+Project.hasMany(Task, 
+    {foreignKey: 'projectId', 
+    sourceKey: 'id'
+});
+
+// Acá dentro del hasMany le decimos que la llave foranea de la tabla Task es projectId 
+// que va vinculada con la llave primaria de la tabla Project es id de esta forma estan relacionadas muchas tareas con un solo proyecto
+
+Task.belongsTo(Project,{
+    foreignKey: 'projectId',
+    targetId: 'id'
+})
