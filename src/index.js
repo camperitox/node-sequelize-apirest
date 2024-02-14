f83f73f674e2fb07d8138112857e0ba06fea23fa
camperitox <ml17017@ues.edu.sv>
2024-02-14 15:56:51 -0600

    Project - created -list

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import app from './app.js';
 import { sequelize } from './database/database.js';
-import './models/Project.js';
-import './models/Task.js';
 
 async function main() {
     try {
-        await sequelize.sync({force:true})// esto crea las tablas si no existen aun 
+        await sequelize.sync({force:false})// esto crea las tablas si no existen aun 
         app.listen(3000)
         console.log('Server on port 3000')
     } catch (error) {
