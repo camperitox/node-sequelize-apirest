0799ae48e40c03581c1dfd8a08e2075d644d8f89
camperitox <ml17017@ues.edu.sv>
2024-02-15 10:53:39 -0600

    se hizo configuracion del corss

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express  from 'express';
+import cors from 'cors';
 import projectsRoutes from './routes/projects.routes.js';
 import tasksRoutes from './routes/tasks.routes.js';
 import taskStatusRoutes from './routes/taskStatus.routes.js';
 
 const app = express();
+
+// Configuración de CORS para permitir solicitudes desde cualquier origen
+app.use(cors());
+
 
 // Middlewares
 app.use(express.json());
